490679dc4b1cda66c44c393502b1d181b25931c1
jschembr <jschembr@ulb.ac.be>
2015-05-14 22:30:14 +0200

    ajout de la partie 9

---
+++ src/scheduler-v9/ScheduleSolver.cpp
@@ -0,0 +1,213 @@
+#include "ScheduleSolver.hpp"
+#include "../minisat/Solver.hpp"
+#include "Parser.hpp"
+#include "Data.hpp"
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+ScheduleSolver::ScheduleSolver(std::string fileName) : parser(fileName) { 
+
+}
+
+void ScheduleSolver::solve() {
+	Data d = this->parser.parse();
+
+	d.print();
+
+	Solver sol;
+	int prop[d.getX()][d.getT()][d.getS()];
+
+	for (int x = 0; x < d.getX(); ++x) {
+		for (int t = 0; t < d.getT(); ++t) {
+			for (int s = 0; s < d.getS(); ++s) {
+				prop[x][t][s] = sol.newVar();
+			}
+		}
+	}
+
+	// Contrainte d'unicité sur les examen, le temps et les salles
+	// -> les examens ne se produisent pas deux fois (dans le temps et l'espace)
+	for (int x = 0; x < d.getX(); ++x) {
+		for (int t = 0; t < d.getT(); ++t) {
+			for (int s = 0; s < d.getS(); ++s) {
+				for (int k = 0; k < d.getX(); ++k) {
+					if (k!=x) {
+						sol.addBinary(~Lit(prop[x][t][s]),~Lit(prop[k][t][s]));
+					}
+				}
+				for (int k = 0; k < d.getT(); ++k) {
+					if (k!=t) {
+						sol.addBinary(~Lit(prop[x][t][s]),~Lit(prop[x][k][s]));
+					}
+				}
+				for (int k = 0; k < d.getS(); ++k) {
+					if (k!=s) {
+						sol.addBinary(~Lit(prop[x][t][s]),~Lit(prop[x][t][k]));
+					}
+				}
+			}
+		}
+	}
+
+	// Contrainte d'existence
+	// -> Il faut que chaque examen soit planifié au moins une fois
+	for (int x = 0; x < d.getX(); ++x) {
+		vec<Lit> lits;
+		for (int t = 0; t < d.getT(); ++t) {
+			for (int s = 0; s < d.getS(); ++s) {
+				lits.push(Lit(prop[x][t][s]));
+			}
+		}
+		sol.addClause(lits);
+	}
+
+	// oblige les n heures qui suivent le début d'un examen de n heures d'être libre
+	for (int x1 = 0; x1 < d.getX(); ++x1) {
+		for (int t = 0; t < d.getT(); ++t) {
+			for (int s = 0; s < d.getS(); ++s) {
+				if (t+d.getD()[x1]-1<d.getT()) {
+					for (int dt = 1; dt < d.getD()[x1]; ++dt) {
+						for (int x2 = 0; x2 < d.getX(); ++x2) {
+							// std::cout << "not " << x1 << " " << t << " " << s << " or not " << x2 << " " << t+dt << " " << s << std::endl;
+							sol.addBinary(~Lit(prop[x1][t][s]),~Lit(prop[x2][t+dt][s]));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// ne peut pas commencer si l'examen sort de l'horraire
+	for (int x = 0; x < d.getX(); ++x) {
+		for (int s = 0; s < d.getS(); ++s) {
+			for (int t = d.getT()-d.getD()[x]+1; t < d.getT(); ++t) {
+				sol.addUnit(~Lit(prop[x][t][s]));
+			}
+		}
+	}
+
+	// Contrainte de la limitation de place dans les salles
+	// -> Un examen ou il y a x personnes ne peut pas avoir lieu dans une salle des n places si n<x
+	for (int x = 0; x < d.getX(); ++x) {
+		for (int t = 0; t < d.getT(); ++t) {
+			for (int s = 0; s < d.getS(); ++s) {
+				if (d.getV()[x]>d.getC()[s]) {
+					sol.addUnit(~Lit(prop[x][t][s]));
+				}
+			}
+		}
+	};
+
+	// Contrainte de la résolution des conflit horraire pour un étudiant
+	// -> Un étudiant ne peut pas avoir 2 examens en même temps
+	for (int e = 0; e < d.getE(); ++e) {
+		for (int x = 0; x < d.getA()[e].size(); ++x) {
+			for (int y = 0; y < d.getA()[e].size(); ++y) {
+				if (x!=y) {
+					for (int t = 0; t < d.getT(); ++t) {
+						for (int s = 0; s < d.getS(); ++s) {
+							for (int s2 = 0; s2 < d.getS(); ++s2) {
+								for (int dt = t-d.getD()[d.getA()[e][y]-1]+1; dt <= t+d.getD()[d.getA()[e][x]-1]-1; ++dt) {
+									if ((dt>=0) && (dt<d.getT())) {
+										// std::cout << "not " << d.getA()[e][x]-1 << " " << t << " " << s << " or not " << d.getA()[e][y]-1 << " " << dt << " " << s2 << std::endl;
+										sol.addBinary(~Lit(prop[d.getA()[e][x]-1][t][s]),~Lit(prop[d.getA()[e][y]-1][dt][s2]));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Contrainte de la résolution des conflit horraire pour un professeur
+	// -> Un professeur ne peut pas surveiller 2 examens en même temps
+	for (int p = 0; p < d.getP(); ++p) {
+		for (int x = 0; x < d.getB()[p].size(); ++x) {
+			for (int y = 0; y < d.getB()[p].size(); ++y) {
+				if (x!=y) {
+					for (int t = 0; t < d.getT(); ++t) {
+						for (int s = 0; s < d.getS(); ++s) {
+							for (int s2 = 0; s2 < d.getS(); ++s2) {
+								for (int dt = t-d.getD()[d.getB()[p][y]-1]+1; dt <= t+d.getD()[d.getB()[p][x]-1]-1; ++dt) {
+									if ((dt>=0) && (dt<d.getT())) {
+										sol.addBinary(~Lit(prop[d.getB()[p][x]-1][t][s]),~Lit(prop[d.getB()[p][y]-1][dt][s2]));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < d.getI().size(); ++i) {
+		int d1(d.getI()[i][0]);
+		int d2(d.getI()[i][1]);
+		for (int x = 0; x < d.getX(); ++x) {
+			for (int s = 0; s < d.getS(); ++s) {
+				for (int t = d1; t < d2+1; ++t) {
+					std::cout << x << " " << t << " " << s << std::endl;
+					sol.addUnit(~Lit(prop[x][t][s]));
+				}
+			}
+		}
+		for (int x = 0; x < d.getX(); ++x) {
+			for (int s = 0; s < d.getS(); ++s) {
+				for (int t = d1-d.getD()[x]+1; t < d1; ++t) {
+					if (t>=0) {
+						std::cout << x << " " << t << " " << s << std::endl;
+						sol.addUnit(~Lit(prop[x][t][s]));
+					}
+				}
+			}
+		}
+	}
+
+	for (int e = 0; e < d.getE(); ++e) {
+		for (int x = 0; x < d.getA()[e].size(); ++x) {
+			for (int y = 0; y < d.getA()[e].size(); ++y) {
+				if (x!=y) {
+					for (int t = 0; t < d.getT(); ++t) {
+						for (int s = 0; s < d.getS(); ++s) {
+							for (int s2 = 0; s2 < d.getS(); ++s2) {
+								int dt(d.getD()[d.getA()[e][x]-1]);
+								if ((s!=s2) && (t+dt<d.getT())) {
+									std::cout << "not " << d.getA()[e][x]-1 << " " << t << " " << s << " or not " << d.getA()[e][y]-1 << " " << t+dt << " " << s2 << std::endl;
+									sol.addBinary(~Lit(prop[d.getA()[e][x]-1][t][s]),~Lit(prop[d.getA()[e][y]-1][t+dt][s2]));
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	sol.solve();
+
+	// print de l'output
+	if (sol.okay()) {
+		for (int x = 0; x < d.getX(); ++x) {
+			for (int t = 0; t < d.getT(); ++t) {
+				for (int s = 0; s < d.getS(); ++s) {
+					if (sol.model[prop[x][t][s]]==l_True) {
+						std::cout << "L'examen " << x << " se tiendra pendant la période " << t << " dans la salle " << s << std::endl;
+					}
+				}
+			}
+		}
+	}
+	else{
+		std::cout << "Ce problème n'a pas de solution."<<std::endl;
+	}
+
+}
+
+ScheduleSolver::~ScheduleSolver(){
+
+}
